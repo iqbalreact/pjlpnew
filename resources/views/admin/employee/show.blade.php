768e84b952b5869dce045d6ef14d7472a1beb156
Ikhsan Nugraha <shofar.ikhsan@gmail.com>
2019-07-21 11:42:21 +0700

    detail employee

--- resources/views/admin/employee/show.blade.php
+++ resources/views/admin/employee/show.blade.php
@@ -17,10 +17,45 @@
                     <img class="profile-user-img img-responsive img-circle" src="{{ $data->getAvatar() }}" alt="User profile picture">
         
                     <h3 class="profile-username text-center">{{ $data->name }}</h3>
+                    <ul class="list-group list-group-unbordered">
+                        <li class="list-group-item">
+                        <b>NIK</b> <a class="pull-right">{{ $data->nik }}</a>
+                    </li>
+                    <li class="list-group-item">
+                        <b>KTP</b> <a class="pull-right">{{ $data->ktp }}</a>
+                    </li>
+                    <li class="list-group-item">
+                        <b>NPWP</b> <a class="pull-right">{{ $data->npwp ?? '-' }}</a>
+                    </li>
+                    </ul>
+
+                    <a href="{{ route('employee.edit', ['id' => $data->id ]) }}" class="btn btn-success btn-block"><i class="fa fa-pencil"></i> <b>Edit</b></a>
     
                     </div>
             </div>
         </div>
+
+        <div class="col-md-9">
+            <div class="nav-tabs-custom">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#about" data-toggle="tab" aria-expanded="true">Tentang</a></li>
+                    <li class=""><a href="#timeline" data-toggle="tab" aria-expanded="false">Timeline</a></li>
+                    <li class=""><a href="#settings" data-toggle="tab" aria-expanded="false">Settings</a></li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="about">
+                        @include('admin.employee.show.about')
+                    </div>
+                    <div class="tab-pane" id="timeline">
+                        
+                    </div>
+        
+                    <div class="tab-pane" id="settings">
+                        
+                    </div>
+                </div>
+            </div>
+            </div>
     </div>
 @stop
 
---
+++ resources/views/admin/employee/show/about.blade.php
@@ -0,0 +1,51 @@
+<form class="form-horizontal">
+    <div class="form-group">
+        <label class="col-sm-2 control-label">NIK</label>
+
+        <div class="col-sm-10">
+            <input class="form-control" value="{{ $data->nik }}" readonly>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Nama</label>
+
+        <div class="col-sm-10">
+            <input class="form-control" value="{{ $data->name }}" readonly>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">No KTP</label>
+
+        <div class="col-sm-10">
+            <input class="form-control" value="{{ $data->ktp }}" readonly>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">NPWP</label>
+
+        <div class="col-sm-10">
+            <input class="form-control" value="{{ $data->npwp }}" readonly>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Rekening</label>
+
+        <div class="col-sm-10">
+            <input class="form-control" value="{{ $data->account_number }}" readonly>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">BPJS Kesehatan</label>
+
+        <div class="col-sm-10">
+            <input class="form-control" value="{{ $data->bpjs_healthcare }}" readonly>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">BPJS Ketenagakerjaan</label>
+
+        <div class="col-sm-10">
+            <input class="form-control" value='{{ $data->bpjs_social_security }}' readonly>
+        </div>
+    </div>
+</form>
