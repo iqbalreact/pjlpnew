28ac8d2dc3455cf0b79987c78c3bbbc40be01914
Ikhsan Nugraha <shofar.ikhsan@gmail.com>
2019-11-24 00:01:13 +0700

    jabatan show

--- resources/views/admin/functionary/show/about.blade.php
+++ resources/views/admin/functionary/show/about.blade.php
@@ -21,4 +21,26 @@
             <input class="form-control" value="{{ $data->phone_number }}" readonly>
         </div>
     </div>
+
+    <hr>
+    <h4>Jabatan</h4>
+
+    @foreach ($data->occupations as $occupation)
+        <div class="form-group">
+            <label class="col-sm-2 control-label">Nama Jabatan</label>
+    
+            <div class="col-sm-10">
+                <input class="form-control" value="{{ $occupation->position_transform ?? ''}}" readonly>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-2 control-label">SKPD</label>
+    
+            <div class="col-sm-10">
+                <input class="form-control" value="{{ $occupation->skpd->name ?? ''  }}" readonly>
+            </div>
+        </div>     
+
+        <hr>
+    @endforeach
 </form>
