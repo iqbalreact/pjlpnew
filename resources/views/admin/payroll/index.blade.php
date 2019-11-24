bdfb03e3bf35e47529e5bfb837a7bebce7891628
Harry Saputra <flexnewbie@gmail.com>
2019-11-24 17:22:11 +0700

    Tweak filter payroll table
    :+1:

--- resources/views/admin/payroll/index.blade.php
+++ resources/views/admin/payroll/index.blade.php
@@ -11,42 +11,46 @@
 @section('content')
     <div class="row">
         <div class="col-xs-12">
-            <div class="box box-primary">
-                <div class="box-header">   
-                    <h3 class="box-title">Filter</h3>
-                </div>
-                <div class="box-body">
-                    <form class="form-horizontal">
-                        <div class="form-group">
-                            <label for="inputWorkPackage" class="col-sm-2 control-label">Paket Pekerjaan @include('components.required')</label>
-                    
-                            <div class="col-sm-10">
-                                <select name="work_package_id" id="workPackageSelect" class="form-control"></select>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputMonth" class="col-sm-2 control-label">Bulan dan Tahun @include('components.required')</label>
-    
-                            <div class="col-sm-10">
-                                {!! Form::text('date', '', ['id' => 'date', 'class' => 'form-control datepicker', 'placeholder'=> __('Bulan dan Tahun'), 'autocomplete' => 'off'] ) !!}
-                            </div>
-                        </div>
-                         <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-10">
-                                <button
-                                    id="findData" 
-                                    type="button" 
-                                    class="btn btn-primary">
-                                        <i class="fa fa-search"></i> Cari
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+
             <div class="box box-primary" id="inputAttendace">
                 <div class="box-header">   
                     <h3 class="box-title">Input Gaji</h3>
+
+
+                    <form class="form-horizontal" style="margin-top:20px;">
+
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="form-group">
+                                    <label for="inputWorkPackage" class="col-sm-3 control-label">Paket Pekerjaan @include('components.required')</label>
+                            
+                                    <div class="col-sm-9">
+                                        <select name="work_package_id" id="workPackageSelect" class="form-control"></select>
+                                    </div>
+                                </div>                               
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    <label for="inputMonth" class="col-sm-4 control-label">Periode @include('components.required')</label>
+            
+                                    <div class="col-sm-8">
+                                        {!! Form::text('date', '', ['id' => 'date', 'class' => 'form-control datepicker', 'placeholder'=> __('Bulan dan Tahun'), 'autocomplete' => 'off'] ) !!}
+                                    </div>
+                                </div>                               
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <button
+                                        id="findData" 
+                                        type="button" 
+                                        class="btn btn-primary">
+                                            <i class="fa fa-search"></i> Cari
+                                    </button>
+                                </div>                               
+                            </div>
+                        </div>
+                    </form>
+
                 </div>
                 <div class="box-body">
                     <table id="employee-table" class="table" width="100%">
