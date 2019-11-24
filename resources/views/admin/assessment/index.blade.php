eea7ec13856ae2fbf3f444ffabc00898b3c757dc
Harry Saputra <flexnewbie@gmail.com>
2019-11-24 14:08:50 +0700

    tweak assessment table
    :P

--- resources/views/admin/assessment/index.blade.php
+++ resources/views/admin/assessment/index.blade.php
@@ -54,15 +54,16 @@
                             <tr>
                                 <th></th>
                                 <th>Id</th>
-                                <th>NIPJ</th>
-                                <th>Name</th>
+                                <th style="width:150px;">NIPJ</th>
+                                <th style="width:250px;">Name</th>
+                                <th>Status</th>
                                 <th style="font-size:80%;">Tingkat Penyelesaian Pekerjaan</th>
                                 <th style="font-size:80%;">Waktu Penyelesaian Pekerjaan</th>
                                 <th style="font-size:80%;">Kualitas Hasil Pekerjaan</th>
                                 <th style="font-size:80%;">Kepatuhan Menjalankan Kewajiban</th>
                                 <th style="font-size:80%;">Kepatuhan terhadap Larangan</th>
                                 <th>Aksi</th>
-                                <th>Status</th>
+                                
                             </tr>
                         </thead>
                     </table> 
@@ -131,13 +132,14 @@
             { data: 'id', name: 'id', class:'hide' },
             { data: 'employee_nipj', name: 'employee.nipj', searchable:'true', orderable:'false'},
             { data: 'employee_name', name: 'employee.name', searchable:'true', orderable:'false'},
-            { data: 'work_completion_rate', name: 'work_completion_rate', searchable:'false', orderable:'false', "width": "10%"},
-            { data: 'work_completion_time', name: 'work_completion_time', searchable:'false', orderable:'false', "width": "10%"},
-            { data: 'work_quality', name: 'work_quality', searchable:'false', orderable:'false', "width": "10%"},
-            { data: 'obidence_on_obligation', name: 'obidence_on_obligation', searchable:'false', orderable:'false', "width": "10%"},
-            { data: 'obidence_on_rule', name: 'obidence_on_rule', searchable:'false', orderable:'false', "width": "10%"},
-            { data: 'save', name: 'save', searchable:'false', orderable:'false', "width": "5%"},
-            { data: 'status', name: 'status', searchable:'false', orderable:'false', "width": "5%"}
+            { data: 'status', name: 'status', searchable:'false', orderable:'false'},
+            { data: 'work_completion_rate', name: 'work_completion_rate', searchable:'false', orderable:'false'},
+            { data: 'work_completion_time', name: 'work_completion_time', searchable:'false', orderable:'false'},
+            { data: 'work_quality', name: 'work_quality', searchable:'false', orderable:'false'},
+            { data: 'obidence_on_obligation', name: 'obidence_on_obligation', searchable:'false', orderable:'false'},
+            { data: 'obidence_on_rule', name: 'obidence_on_rule', searchable:'false', orderable:'false'},
+            { data: 'save', name: 'save', searchable:'false', orderable:'false'}
+            
         ]
     });
 
@@ -169,13 +171,13 @@
         
         var employee_id = data.employee.id;
         var contract_id = data.id;
-        var work_completion_rate    = oTable.api().cell(idx,4).nodes().to$().find('select').val();
-        var work_completion_time    = oTable.api().cell(idx,5).nodes().to$().find('select').val();
-        var work_quality            = oTable.api().cell(idx,6).nodes().to$().find('select').val();
-        var obidence_on_obligation  = oTable.api().cell(idx,7).nodes().to$().find('select').val();
-        var obidence_on_rule        = oTable.api().cell(idx,8).nodes().to$().find('select').val();
+        var work_completion_rate    = oTable.api().cell(idx,5).nodes().to$().find('select').val();
+        var work_completion_time    = oTable.api().cell(idx,6).nodes().to$().find('select').val();
+        var work_quality            = oTable.api().cell(idx,7).nodes().to$().find('select').val();
+        var obidence_on_obligation  = oTable.api().cell(idx,8).nodes().to$().find('select').val();
+        var obidence_on_rule        = oTable.api().cell(idx,9).nodes().to$().find('select').val();
                
-        oTable.api().cell(idx, (10)).nodes().to$().find('.stateStatus').html("<img src='/img/spinner.gif'>");
+        oTable.api().cell(idx, (4)).nodes().to$().find('.stateStatus').html("<img src='/img/spinner.gif'>");
 
         $.post('{{ route('assessment.store') }}', {
             employee_id: employee_id, 
@@ -189,9 +191,9 @@
             obidence_on_rule: obidence_on_rule
         }, function(data, status) {
             if (status == 'success') {
-                oTable.api().cell(idx, 10).nodes().to$().find('.stateStatus').html("<img src='/img/checked.png'>");
+                oTable.api().cell(idx, 4).nodes().to$().find('.stateStatus').html("<img src='/img/checked.png'>");
             } else {
-                oTable.api().cell(idx, 10).nodes().to$().find('.stateStatus').html("<img src='/img/cancel.png'>");
+                oTable.api().cell(idx, 4).nodes().to$().find('.stateStatus').html("<img src='/img/cancel.png'>");
             }
         });
     }
