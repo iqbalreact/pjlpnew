5b2dae1759f8ad1382f4867e111c31b94a7c5c5f
Harry Saputra <flexnewbie@gmail.com>
2019-11-25 06:25:12 +0700

    Update label Nama Lokasi

--- resources/views/admin/location/form.blade.php
+++ resources/views/admin/location/form.blade.php
@@ -28,7 +28,7 @@
     @endif
 
     <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
-        <label for="inputName" class="col-sm-2 control-label">Nama @include('components.required')</label>
+        <label for="inputName" class="col-sm-2 control-label">Nama Lokasi @include('components.required')</label>
 
         <div class="col-sm-10">
             {!! Form::text('name', $edit ? $data->name : old('name'), ['class' => 'form-control', 'placeholder'=> __('Nama lokasi')] ) !!}
