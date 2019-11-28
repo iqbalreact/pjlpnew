8a476f57d09ae3b2e1bfabde4426722ec3589f38
Harry Saputra <flexnewbie@gmail.com>
2019-11-28 08:52:34 +0700

    tab penilaian @ pjlp details page

---
+++ resources/views/admin/employee/show/assessment.blade.php
@@ -0,0 +1,106 @@
+<style type="text/css">
+	.dl-horizontal dt {
+		width:50px !important;
+	}
+	.dl-horizontal dd {
+		margin-left: 70px !important;
+	}
+</style>
+
+<table class="table table-default table-bordered">
+	<thead>
+		<tr>
+			<th>Bulan</th>
+			<th>Tahun</th>
+			<th>Status</th>
+			<th>TPP</th>
+			<th>WPP</th>
+			<th>KHP</th>
+			<th>KMK</th>
+			<th>KTL</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>November</td>
+			<td>2019</td>
+			<td class="text-success">Lengkap</td>
+			<td>4</td>
+			<td>4</td>
+			<td>3</td>
+			<td>4</td>
+			<td>4</td>
+		</tr>
+		<tr>
+			<td>Juli</td>
+			<td>2019</td>
+			<td class="text-success">Lengkap</td>
+			<td>4</td>
+			<td>4</td>
+			<td>3</td>
+			<td>4</td>
+			<td>4</td>
+		</tr>
+		<tr>
+			<td>Agustus</td>
+			<td>2019</td>
+			<td class="text-success">Lengkap</td>
+			<td>4</td>
+			<td>4</td>
+			<td>3</td>
+			<td>4</td>
+			<td>4</td>
+		</tr>
+		<tr>
+			<td>September</td>
+			<td>2019</td>
+			<td class="text-success">Lengkap</td>
+			<td>4</td>
+			<td>4</td>
+			<td>3</td>
+			<td>4</td>
+			<td>4</td>
+		</tr>
+		<tr>
+			<td>Oktober</td>
+			<td>2019</td>
+			<td class="text-success">Lengkap</td>
+			<td>4</td>
+			<td>4</td>
+			<td>3</td>
+			<td>4</td>
+			<td>4</td>
+		</tr>
+		<tr>
+			<td>November</td>
+			<td>2019</td>
+			<td class="text-success">Lengkap</td>
+			<td>4</td>
+			<td>4</td>
+			<td>3</td>
+			<td>4</td>
+			<td>4</td>
+		</tr>
+	</tbody>
+</table>
+
+<div class="row">
+	<div class="col-md-6">
+		<dl class="dl-horizontal">
+			<dt>TPP</dt>
+			<dd>Tingkat Penyelesaian Pekerjaan</dd>
+			<dt>WPP</dt>
+			<dd>Waktu Penyelesaian Pekerjaan</dd>
+			<dt>KHP</dt>
+			<dd>Kualitas Hasil Pekerjaan</dd>
+		</dl>
+	</div>
+	<div class="col-md-6">
+		<dl class="dl-horizontal">
+			<dt>KMK</dt>
+			<dd>Kepatuhan Menjalankan Kewajiban</dd>
+			<dt>KTL</dt>
+			<dd>Kepatuhan Terhadap Larangan</dd>
+		</dl>		
+	</div>
+</div>	
